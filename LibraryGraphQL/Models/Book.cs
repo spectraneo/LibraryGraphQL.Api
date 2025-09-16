3e82dc597791ae8198e1e344aac05023ae2ae542
Nicolas Anderson <nicolas-31@outlook.com>
2025-09-16 16:11:18 +0200

    Added a comment on Book model

--- LibraryGraphQL/Models/Book.cs
+++ LibraryGraphQL/Models/Book.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Title { get; set; }
          = string.Empty;
+
+        // Foreign key + navigation back to Author
         public int AuthorId { get; set; }
         public Author Author { get; set; } = null!;
     }
